48a50d77c68c439cf8855e95d085058f297fc40a
thomas <thomas@dooms.eu>
2022-07-07 11:48:38 +0200

    cropper fixed border size

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,35 @@ macro_rules! callback {
     };
 }
 
-const V_BORDER: f64 = 40.0;
-const H_BORDER: f64 = 30.0;
-
 struct CenterImage {
     offset: (f64, f64),
     dims: (f64, f64),
     scale: f64,
 }
 
-fn center_image(canvas: &HtmlCanvasElement, image: &HtmlImageElement, zoom: f64) -> CenterImage {
-    let (i_width, i_height) = (image.width() as f64, image.height() as f64);
-    let (c_width, c_height) = (canvas.width() as f64, canvas.height() as f64);
-    let (l_width, l_height) = (c_width - 2.0 * V_BORDER, c_height - 2.0 * H_BORDER);
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Dimensions {
+    pub image_dims: (f64, f64),
+    pub canvas_dims: (f64, f64),
+    pub borders: (f64, f64),
+    pub ratio: f64,
+}
 
+impl Dimensions {
+    pub fn new(canvas: &HtmlCanvasElement, image: &HtmlImageElement) -> Self {
+        let image_dims = (image.width() as f64, image.height() as f64);
+        let canvas_dims = (canvas.width() as f64, canvas.height() as f64);
+        let borders = (canvas_dims.0 / 20.0, canvas_dims.1 / 20.0);
+        let ratio = canvas_dims.0 / canvas_dims.1;
+
+        Self { image_dims, canvas_dims, borders, ratio }
+    }
+}
+
+fn center_image(dims: Dimensions, zoom: f64) -> CenterImage {
+    let Dimensions { image_dims: (i_width, i_height), canvas_dims: (c_width, c_height), borders, .. } = dims;
+
+    let (l_width, l_height) = (c_width - 2.0 * borders.0, c_height - 2.0 * borders.1);
     let scale = (l_width / i_width).max(l_height / i_height) * zoom;
 
     let dims = (i_width * scale, i_height * scale);
@@ -36,8 +51,8 @@ fn center_image(canvas: &HtmlCanvasElement, image: &HtmlImageElement, zoom: f64)
     CenterImage { dims, offset, scale }
 }
 
-fn constrain_position((pos_x, pos_y): (f64, f64), (off_x, off_y): (f64, f64)) -> (f64, f64) {
-    let (x_win, y_win) = (V_BORDER - off_x, H_BORDER - off_y);
+fn constrain_position(dims: Dimensions, (pos_x, pos_y): (f64, f64), (off_x, off_y): (f64, f64)) -> (f64, f64) {
+    let (x_win, y_win) = (dims.borders.0 - off_x, dims.borders.1 - off_y);
 
     let new_x = if x_win != 0.0 { pos_x.clamp(-x_win.abs(), x_win.abs()) } else { 0.0 };
     let new_y = if y_win != 0.0 { pos_y.clamp(-y_win.abs(), y_win.abs()) } else { 0.0 };
@@ -45,16 +60,15 @@ fn constrain_position((pos_x, pos_y): (f64, f64), (off_x, off_y): (f64, f64)) ->
     (new_x, new_y)
 }
 
-fn bounding_box(image: &HtmlImageElement, (pos_x, pos_y): (f64, f64), scale: f64, zoom: f64) -> ((f64, f64), (f64, f64)) {
-    let (width, height) = (image.width() as f64, image.height() as f64);
-    let factor = height.min( 3.0/4.0 * width);
+fn bounding_box(dims: Dimensions, (pos_x, pos_y): (f64, f64), scale: f64, zoom: f64) -> ((f64, f64), (f64, f64)) {
+    let Dimensions { image_dims: (width, height), ratio, .. } = dims;
 
-    let dims = (4.0/3.0 * factor / zoom, factor / zoom);
+    let factor = height.min(width / ratio);
+    let dims = (ratio * factor / zoom, factor / zoom);
 
     let (x_center, y_center) = (pos_x / scale + width / 2.0, pos_y / scale + height / 2.0);
 
-
-    let x_corner = x_center - 4.0/3.0 * factor / (2.0 * zoom);
+    let x_corner = x_center - 4.0 / 3.0 * factor / (2.0 * zoom);
     let y_corner = y_center - factor / (2.0 * zoom);
 
     ((x_corner, y_corner), dims)
@@ -64,7 +78,10 @@ fn draw(canvas: HtmlCanvasElement, image: HtmlImageElement, zoom: f64, pos: (f64
     let element = canvas.get_context("2d").unwrap().unwrap();
     let context = element.dyn_into::<CanvasRenderingContext2d>().unwrap();
 
-    let CenterImage { dims, offset, scale } = center_image(&canvas, &image, zoom);
+    let dimensions = Dimensions::new(&canvas, &image);
+    let (v_border, h_border) = dimensions.borders;
+
+    let CenterImage { dims, offset, scale } = center_image(dimensions, zoom);
     let (width, height) = (canvas.width() as f64, canvas.height() as f64);
 
     context.set_image_smoothing_enabled(true);
@@ -85,14 +102,14 @@ fn draw(canvas: HtmlCanvasElement, image: HtmlImageElement, zoom: f64, pos: (f64
     context.set_global_alpha(0.5);
 
     context.begin_path();
-    context.move_to(V_BORDER, H_BORDER + radius); // start top-left
-    context.arc(V_BORDER + radius, H_BORDER + radius, radius, PI, 1.5 * PI).unwrap();
-    context.line_to(width - V_BORDER - radius, H_BORDER); // top-right of image
-    context.arc(width - V_BORDER - radius, H_BORDER + radius, radius, 1.5 * PI, 0.0).unwrap();
-    context.line_to(width - V_BORDER, height - H_BORDER - radius); // bottom-right of image
-    context.arc(width - V_BORDER - radius, height - H_BORDER - radius, radius, 0.0, 0.5 * PI).unwrap();
-    context.line_to(V_BORDER + radius, height - H_BORDER); // bottom-left of image
-    context.arc(V_BORDER + radius, height - H_BORDER - radius, radius, 0.5 * PI, PI).unwrap();
+    context.move_to(v_border, h_border + radius); // start top-left
+    context.arc(v_border + radius, h_border + radius, radius, PI, 1.5 * PI).unwrap();
+    context.line_to(width - v_border - radius, h_border); // top-right of image
+    context.arc(width - v_border - radius, h_border + radius, radius, 1.5 * PI, 0.0).unwrap();
+    context.line_to(width - v_border, height - h_border - radius); // bottom-right of image
+    context.arc(width - v_border - radius, height - h_border - radius, radius, 0.0, 0.5 * PI).unwrap();
+    context.line_to(v_border + radius, height - h_border); // bottom-left of image
+    context.arc(v_border + radius, height - h_border - radius, radius, 0.5 * PI, PI).unwrap();
     context.close_path();
 
     context.rect(width, 0.0, -width, height);
@@ -128,6 +145,7 @@ pub fn cropper(props: &Props) -> Html {
     let zoom = use_state_eq(|| 1.0);
     let position = use_state(|| (0.0, 0.0));
     let clicked = use_state(|| None);
+
     let canvas = use_node_ref();
 
     let (canvas_c, image_c, position_c, zoom_c) = (canvas.clone(), image.clone(), position.clone(), zoom.clone());
@@ -137,8 +155,9 @@ pub fn cropper(props: &Props) -> Html {
     });
 
     let onchange = callback!(zoom, position, canvas, image; move |value| {
-        let CenterImage{offset, ..} = center_image(&canvas.cast().unwrap(), &*image.clone(), *zoom);
-        let pos = constrain_position(*position, offset);
+        let dims = Dimensions::new(&canvas.cast().unwrap(), &*image);
+        let CenterImage{offset, ..} = center_image(dims, *zoom);
+        let pos = constrain_position(dims, *position, offset);
 
         position.set(pos);
         zoom.set(value);
@@ -155,8 +174,9 @@ pub fn cropper(props: &Props) -> Html {
         if let Some((start_x, start_y)) = *clicked {
             let new = (position.0 - absolute.0 + start_x, position.1 - absolute.1 + start_y);
 
-            let CenterImage{offset, ..} = center_image(&canvas.cast().unwrap(), &*image.clone(), *zoom);
-            let pos = constrain_position(new, offset);
+            let dims = Dimensions::new(&canvas.cast().unwrap(), &*image);
+            let CenterImage{offset, ..} = center_image(dims, *zoom);
+            let pos = constrain_position(dims, new, offset);
 
             clicked.set(Some(absolute));
             position.set(pos);
@@ -170,8 +190,9 @@ pub fn cropper(props: &Props) -> Html {
         let element = canvas.get_context("2d").unwrap().unwrap();
         let context = element.dyn_into::<CanvasRenderingContext2d>().unwrap();
 
-        let CenterImage{scale, ..} = center_image(&canvas, &*image.clone(), *zoom);
-        let bb = bounding_box(&*image.clone(), *position, scale, *zoom);
+        let dims = Dimensions::new(&canvas, &*image);
+        let CenterImage{scale, ..} = center_image(dims, *zoom);
+        let bb = bounding_box(dims, *position, scale, *zoom);
 
         context.clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
         context.set_global_alpha(1.0);
@@ -183,14 +204,14 @@ pub fn cropper(props: &Props) -> Html {
             bb.1.1,
             0.0,
             0.0,
-            400.0,
-            300.0,
+            canvas.width() as f64,
+            canvas.height() as f64,
         ).unwrap();
 
         callback.emit(Some(canvas.to_data_url().unwrap()))
     });
 
-    let footer = html!{
+    let footer = html! {
         <Buttons>
         <Button onclick={onclose.clone()}> <span> {"Cancel"} </span> </Button>
         <Button color={Color::Primary} onclick={ondone}> <span> {"Save"} </span> </Button>
